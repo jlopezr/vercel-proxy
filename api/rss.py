2394099c6aa5f210ac11cc10a783df2a48b56d26
Juan López Rubio <j.lopez.r@gmail.com>
2024-09-26 08:15:06 +0200

    PC

--- api/rss.py
+++ api/rss.py
@@ -1,1 +1,12 @@
-from ../lib/europapress-rss import get_merged_feed
+from lib.europapress_rss import get_merged_feed
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        rss_content = get_merged_feed()
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/rss+xml')
+        self.end_headers()
+        self.wfile.write(rss_content.encode('utf-8'))
+        return
